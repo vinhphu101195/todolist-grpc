0c3bd387ce91c61157e2080bd8ef92956d95716f
Phu Chau <vinhphu101195@gmail.com>
2020-05-21 16:17:37 +0700

    add register and get all user in client

--- client/main.go
+++ client/main.go
@@ -5,17 +5,27 @@ import (
 	"net/http"
 	"strconv"
 	"todo-grpc/todoList/proto"
+	"todo-grpc/user/userproto"
 
 	"github.com/gin-gonic/gin"
 	"google.golang.org/grpc"
 )
 
 func main() {
+	// connect todoList
 	conn, err := grpc.Dial("localhost:8000", grpc.WithInsecure())
 	if err != nil {
 		panic(err)
 	}
-	client := proto.NewTodoModelServiceClient(conn)
+	todoClient := proto.NewTodoModelServiceClient(conn)
+
+	// connect User
+	connUser, errUser := grpc.Dial("localHost:8001", grpc.WithInsecure())
+	if errUser != nil {
+		panic(err)
+	}
+	UserClient := userproto.NewUserModelServiceClient(connUser)
+
 	r := gin.Default()
 
 	v1 := r.Group("/api/v1/todos")
@@ -24,7 +34,7 @@ func main() {
 			completed, _ := strconv.Atoi(c.PostForm("completed"))
 			userid, _ := strconv.Atoi(c.PostForm("userid"))
 			req := &proto.CreateRequest{ToDo: &proto.TodoModel{Title: c.PostForm("title"), Completed: int32(completed), Userid: int32(userid)}}
-			if response, err := client.Create(c, req); err == nil {
+			if response, err := todoClient.Create(c, req); err == nil {
 				c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "message": "Todo item created successfully!", "response": response.KeyCreated})
 			} else {
 				c.JSON(http.StatusCreated, gin.H{"error": err.Error()})
@@ -32,7 +42,7 @@ func main() {
 		})
 		v1.GET("/", func(c *gin.Context) {
 			req := &proto.ReadAllRequest{}
-			if response, err := client.ReadAll(c, req); err == nil {
+			if response, err := todoClient.ReadAll(c, req); err == nil {
 				c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": response.ToDos})
 			} else {
 				c.JSON(http.StatusCreated, gin.H{"error": err.Error()})
@@ -41,7 +51,7 @@ func main() {
 		v1.GET("/:id", func(c *gin.Context) {
 			todoID := c.Param("id")
 			req := &proto.ReadRequest{Id: todoID}
-			if response, err := client.Read(c, req); err == nil {
+			if response, err := todoClient.Read(c, req); err == nil {
 				c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": response.ToDo})
 			} else {
 				c.JSON(http.StatusCreated, gin.H{"error": err.Error()})
@@ -51,7 +61,7 @@ func main() {
 			todoID, _ := strconv.Atoi(c.Param("id"))
 			completed, _ := strconv.Atoi(c.PostForm("completed"))
 			req := &proto.UpdateRequest{ToDo: &proto.TodoModel{Id: int32(todoID), Title: c.PostForm("title"), Completed: int32(completed)}}
-			if response, err := client.Update(c, req); err == nil {
+			if response, err := todoClient.Update(c, req); err == nil {
 				c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": response.MessageUdated})
 			} else {
 				c.JSON(http.StatusCreated, gin.H{"error": err.Error()})
@@ -60,8 +70,31 @@ func main() {
 		v1.DELETE("/:id", func(c *gin.Context) {
 			todoID, _ := strconv.Atoi(c.Param("id"))
 			req := &proto.DeleteRequest{Id: int32(todoID)}
-			if response, err := client.Delete(c, req); err == nil {
+			if response, err := todoClient.Delete(c, req); err == nil {
 				c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "message": response.MessageDeleted})
+			} else {
+				c.JSON(http.StatusCreated, gin.H{"error": err.Error()})
+			}
+		})
+	}
+
+	user := r.Group("/user")
+	{
+		user.POST("/", func(c *gin.Context) {
+			username := c.PostForm("username")
+			password := c.PostForm("password")
+			email := c.PostForm("email")
+			req := &userproto.RegisterRequest{User: &userproto.UserModel{Username: username, Password: password, Email: email}}
+			if response, err := UserClient.Register(c, req); err == nil {
+				c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated, "message": "User Register successfully!", "response": response.KeyRegister})
+			} else {
+				c.JSON(http.StatusCreated, gin.H{"error": err.Error()})
+			}
+		})
+		user.GET("/", func(c *gin.Context) {
+			req := &userproto.GetAllUserRequest{}
+			if response, err := UserClient.GetAllUser(c, req); err == nil {
+				c.JSON(http.StatusOK, gin.H{"status": http.StatusOK, "data": response.Users})
 			} else {
 				c.JSON(http.StatusCreated, gin.H{"error": err.Error()})
 			}
